6e768533f69ecf29c7dc48ae1642776376840386
Gabriel Souza <92094301+gabrielsl11@users.noreply.github.com>
2024-11-18 12:36:36 -0300

    Update

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,15 +1,22 @@
-import { Github } from 'lucide-react'
+import { ChevronLeftIcon, Github } from 'lucide-react'
 import Title from '../components/Title'
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 function Details() {
+    let navigate = useNavigate()
     let [searchParams] = useSearchParams()
     let title = searchParams.get("title")
     let description = searchParams.get("description")
 
     return (
         <div class='h-screen w-screen flex flex-col items-center space-y-8 mt-12 bg-slate-900'>
-            <Title>TODO LIST</Title>
+            <div className='flex items-center justify-center relative w-1/3'>
+                <Title>TODO LIST</Title>
+                <ChevronLeftIcon
+                onClick={() => navigate(-1)}
+                className='absolute left-0 top-1/5 cursor-pointer text-slate-100'
+                />
+            </div>  
 
             <main className='flex flex-col justify-center w-1/3 p-4 space-y-2 rounded-md bg-slate-50'>
                 <h2 className='text-xl text-center font-medium text-slate-900'>{title}</h2>
